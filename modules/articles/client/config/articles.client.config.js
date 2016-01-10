e0ac03f4cef2482930e25c202a517d27553f0503
goodsonwebdesign <goodsonwebdesign@gmail.com>
2016-01-10 01:28:07 -0500

    Initial Commit

---
+++ modules/articles/client/config/articles.client.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Configuring the Articles module
+angular.module('articles').run(['Menus',
+  function (Menus) {
+    // Add the articles dropdown item
+    Menus.addMenuItem('topbar', {
+      title: 'What\'s Happening!',
+      state: 'articles',
+      type: 'dropdown',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    Menus.addSubMenuItem('topbar', 'articles', {
+      title: 'Recent News',
+      state: 'articles.list'
+    });
+   
+    // Add the dropdown create item
+    Menus.addSubMenuItem('topbar', 'articles', {
+      title: 'Create Articles',
+      state: 'articles.create',
+      roles: ['user']
+    });
+  }
+]);
+
---
+++ modules/core/client/controllers/home.client.controller.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('core').controller('HomeController', ['$scope', 'Authentication',
+  function ($scope, Authentication) {
+      // This provides Authentication context.
+      $scope.authentication = Authentication;
+
+      var data = [
+         { image: 'modules/core/client/img/brand/logo_2_4.png' },
+         { image: 'modules/core/client/img/brand/logo_2_4.png' },
+         { image: 'modules/core/client/img/brand/logo_2_4.png' }
+      ];
+      function initGalleria() {
+          
+          if (angular.isDefined(Galleria)) {
+              Galleria.run('.galleria', {
+                  dataSource: data,
+              });
+          }
+      }
+      initGalleria();
+
+  }
+
+]);
